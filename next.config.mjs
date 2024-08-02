4990c24d44f3a67303fae908ced26da24cef902a
guusdias <gustavohd34@gmail.com>
2024-08-02 17:35:51 -0300

    fix: login page styles and nextjs configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// next.config.mjs
-const nextConfig = {
-  experimental: {
-    pprPreview: true, // Mantenha esta linha se estiver usando a funcionalidade experimental
-  },
-  // Outras configurações
-};
+const nextConfig = {};
 
 export default nextConfig;
