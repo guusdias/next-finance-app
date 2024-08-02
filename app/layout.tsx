2fefa1b34cc2b45e9d9d4e47c6c5e2faa8ccc2d8
guusdias <gustavohd34@gmail.com>
2024-08-02 15:55:43 -0300

    feat: add metadata on few pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Acme</title>
+      <meta
+        name="description"
+        content="The right place to calculate and have prospections of your invoices."
+      />
+      <meta property="og:title" content="Acme" />
+      <meta property="og:description" content="Finances App" />
+      <meta property="og:image" content="./app/ui/acme-logo" />
+      <meta name="keywords" content="payments, invoices, money" />
+      <link rel="icon" href="./app/ui/acme-logo" />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
