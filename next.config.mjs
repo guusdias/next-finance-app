2fcb9a71f0ad9c9b0a590b47187a41b2ce93c9a4
guusdias <gustavohd34@gmail.com>
2024-08-02 15:59:20 -0300

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 // next.config.mjs
-export default {
+const nextConfig = {
   experimental: {
     pprPreview: true, // Mantenha esta linha se estiver usando a funcionalidade experimental
   },
   // Outras configurações
 };
+
+export default nextConfig;
